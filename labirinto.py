c4d17225a81e3fb1ac93cb096470435b05c12987
Calebe Rebello <caleberoberto2@gmail.com>
2018-08-31 12:50:22 -0400

    Atualizando

--- labirinto.py
+++ labirinto.py
@@ -6,59 +6,30 @@ def criar_matriz(m,n):
 	matriz = []
 	for i in range(m):
 		linha = []
-		for i in range(n):
+		for j in range(n):
 			r = randint(0,1)
 			linha.append(r)
+			if r == 1:
+				matriz[i][j] = -2
+			else:
+				matriz[i][j] = -1
 		matriz.append(linha)
 	return matriz
 
-def empilha(lista, posterior):
-	lista.append(posterior)
-	print(lista)
+def labirinto(matriz,deltaL,deltaC,Li,Ci,Lf,Cf):
+	if((Li == Lf) and (Ci == Cf)):
+		return matriz[Li][Ci]
 
-#def desempilha(lista, posterior):
-#	lista.pop(posterior)
-	
-def movimenta(matriz):
-	atual = matriz[0][0]
-	while(atual != matriz[9][9]):
-		for i in range(9):
-			for j in range(9):
-				if matriz[i][j] == 0:
-					matriz[i][j] = "x"
-					#empilha(lista)
-				elif matriz[i+1][j] == 0:
-					matriz[i][j] = "x"
-					#empilha(lista)
-				elif matriz[i][j-1] == 0:
-					matriz[i][j] = "x"
-					#empilha(lista)
-				elif matriz[i-1][j] == 0:
-					matriz[i][j] = "x"
-					#empilha(lista)
-				else:
-					volta(matriz)					
-		return matriz
-
-def volta(matriz):
-	atual = 0
-	posterior = 0
-	for i in range(9):
-		for j in range(9):
-			if matriz[i][j+1] == 1:
-				posterior = atual
-				#desempilha(lista, posterior)
-			elif matriz[i+1][j] == 1:
-				posterior = atual
-				#desempilha(lista, posterior)
-			elif matriz[i][j-1] == 1:
-				posterior = atual
-				#desempilha(lista, posterior)
-			elif matriz[i-1][j] == 1:
-				posterior = atual
-				#desempilha(lista, posterior)
+	for i in range(4):
+		L = Li + deltaL[i]
+		C = Ci + deltaC[i]
+		if matriz[L][C] == -1:
+			matriz[L][C] = matriz[Li][Ci] + 1
+			caminho = labirinto(matriz, deltaL, deltaC, L, C, Lf, Cf)
+			if caminho > 0:
+				return caminho
 
 m = 10
 n = 10
-labirinto = criar_matriz(m,n)
-print(movimenta(labirinto))
+deltaL = [0, +1, 0, -1]
+deltaC = [+1, 0, -1, 0]
